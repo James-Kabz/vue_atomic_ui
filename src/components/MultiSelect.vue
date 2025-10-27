0d2a8bbb13ed72010f4f12c4db46e70319702f6e
James-Kabz <kabogp@gmail.com>
2025-10-27 20:57:21 +0300

    fix:added badge for selected labels in the multi select field

--- src/components/MultiSelect.vue
+++ src/components/MultiSelect.vue
@@ -12,8 +12,18 @@
       @click="toggleDropdown"
       @keydown="handleKeydown"
     >
-      <span v-if="selectedLabels.length === 0" class="text-gray-500">{{ placeholder || 'Select options' }}</span>
-      <span v-else class="text-gray-900">{{ selectedLabels.join(', ') }}</span>
+      <div v-if="selectedLabels.length === 0" class="text-gray-500">{{ placeholder || 'Select options' }}</div>
+      <div v-else class="flex flex-wrap gap-1">
+        <Badge
+          v-for="label in selectedLabels"
+          :key="label"
+          variant="primary"
+          size="sm"
+          class="mr-1 mb-1"
+        >
+          {{ label }}
+        </Badge>
+      </div>
       <svg class="absolute right-2 top-2.5 h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
         <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
       </svg>
@@ -45,6 +55,7 @@
 
 <script setup>
 import { ref, computed } from 'vue'
+import Badge from './Badge.vue'
 
 const props = defineProps({
   modelValue: {
