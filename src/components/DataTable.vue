94977df92d51d4a89889b3a2a999b8e78ea419cb
James-Kabz <kabogp@gmail.com>
2025-11-03 18:06:46 +0300

    fix:modal styling

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -964,8 +964,8 @@ defineExpose({
       @close="closeModal"
     >
       <div class="p-6">
-        <h3 class="text-lg font-semibold mb-4">Full Text</h3>
-        <div class="text-sm text-gray-700 whitespace-pre-wrap break-words">
+        <h3 class="text-md font-semibold mb-4">Full Text</h3>
+        <div class="text-xl font-bold text-gray-700 whitespace-pre-wrap break-words">
           {{ modalContent }}
         </div>
       </div>
